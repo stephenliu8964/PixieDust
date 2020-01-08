82fe4681a583278c688b0e68f3af6f374def6723
Stephen Liu <dq89_64@hotmail.com>
2020-01-08 17:50:12 +1100

    Updated mdapioutput for the previous commits

---
+++ mdapioutput/applications/Transformation_Engine_Demo.app
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<CustomApplication xmlns="http://soap.sforce.com/2006/04/metadata">
+    <brand>
+        <headerColor>#0070D2</headerColor>
+        <shouldOverrideOrgTheme>false</shouldOverrideOrgTheme>
+    </brand>
+    <formFactors>Large</formFactors>
+    <isNavAutoTempTabsDisabled>true</isNavAutoTempTabsDisabled>
+    <isNavPersonalizationDisabled>true</isNavPersonalizationDisabled>
+    <label>Transformation Engine Demo</label>
+    <navType>Standard</navType>
+    <tabs>Json_to_SObject_Import_Demo</tabs>
+    <tabs>Foo__c</tabs>
+    <tabs>Bar__c</tabs>
+    <tabs>Bar_s_Parent__c</tabs>
+    <uiType>Lightning</uiType>
+    <utilityBar>Transformation_Engine_Demo_UtilityBar</utilityBar>
+</CustomApplication>
---
+++ mdapioutput/lwc/jsonToSObjectImport/jsonToSObjectImport.js
@@ -0,0 +1,120 @@
+/* global Papa */
+import { LightningElement, track } from 'lwc';
+import startImportUpsert from '@salesforce/apex/JsonToSObjectImportController.startImportUpsert';
+import { loadScript } from 'lightning/platformResourceLoader';
+import getPapa from '@salesforce/resourceUrl/Papaparse';
+
+export default class SkillSelectImport extends LightningElement {
+    @track loading;
+    @track dmlResult;
+    @track dmlFailedReason;
+    @track sObjectName;
+    @track externalId;
+    @track errorMessage;
+
+    @track recordLoaded = 0;
+    @track objsString;
+    @track headers = [];
+    @track mapOfListValues = [];
+
+    connectedCallback(){
+    }
+
+    papaInitialized = false;
+    renderedCallback() {
+        if (this.papaInitialized) {
+            return;
+        }
+        this.papaInitialized = true;
+
+        Promise.all([
+            loadScript(this, getPapa),
+        ])
+            .then(() => {
+                console.log('Papaparse loaded');
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
+    }
+
+    handleSObjectName(event){
+        this.sObjectName = event.detail.value;
+    }
+
+    handleExternalId(event){
+        this.externalId = event.detail.value;
+    }
+
+    handleTextAreaInput(event) {
+        let textAreaInput = this.template.querySelector('.textAreaInput');
+        textAreaInput.value = null;
+        this.mapOfListValues = [];
+
+        if(event.detail.value) {
+            let tabString = event.detail.value;
+
+            Papa.parsePromise = function(a) { // first time parsing, returning a JSON String to pass to Apex
+                return new Promise(function(complete, error) {
+                    Papa.parse(a, {header:true, complete, error});
+                });
+            };
+            Papa.parsePromise(
+                tabString
+            ).then(results =>  {
+                let resultData = results.data;
+                resultData.pop();
+                this.objsString = JSON.stringify(resultData);
+            });
+
+            // 2nd parse: for displaying the preview table
+            Papa.parsePromise = function(b) {
+                return new Promise(function(complete, error) {
+                    Papa.parse(b, {complete, error});
+                });
+            };
+            Papa.parsePromise(
+                tabString
+            ).then(results =>  {
+                let resultData = [];
+                resultData = results.data.slice(1);
+                for(let key in resultData) {
+                    if (resultData.hasOwnProperty(key)) {
+                        this.mapOfListValues.push({key: key, value: resultData[key]});
+                    }
+                }
+                this.headers = results.data[0];
+                this.recordLoaded = this.mapOfListValues.length - 1;
+                this.dmlResult = null;
+                this.loading = false;
+            });
+        }
+    }
+
+    handleImport() {
+        console.log(this.sObjectName);
+        console.log(this.externalId);
+        console.log(this.objsString);
+        if (this.sObjectName && this.objsString) {
+            this.loading = true;
+            startImportUpsert({
+                sObjectName: this.sObjectName,
+                jsonString: this.objsString,
+                externalId: this.externalId,
+            }).then(result => {
+                this.loading = false;
+                this.dmlResult = result.dmlResult;
+                this.dmlFailedReason = result.dmlResult === 'Error'? result.dmlFailedReason : null;
+                this.objsString = null;
+                this.mapOfListValues = [];
+                this.headers = [];
+            }).catch(error => {
+                this.loading = false;
+                console.error(error.message);
+            });
+        } else {
+            this.errorMessage = "SObject API Name and data cannot be blank";
+        }
+    }
+
+}
